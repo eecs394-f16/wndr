6d422494c770419c6c0f6e49a0770f34b77af8d8
shikai93 <nsk93@hotmail.com>
2016-11-01 11:36:53 +0800

    added icons, default to happy icon when showing current position

--- app/wndr/scripts/indexController.js
+++ app/wndr/scripts/indexController.js
@@ -1,12 +1,15 @@
 angular
   .module('wndr')
-  .controller('indexController', function($scope, supersonic, $timeout) {
+  .controller('indexController', function($scope, supersonic) {
     
-    supersonic.device.geolocation.getPosition().then( function(position){
+    var init = function () {
+      
+        supersonic.device.geolocation.getPosition().then( function(position){
         $scope.position = position;
         supersonic.logger.debug (position);
         $scope.initMap();
-    });
+      });
+    };
     
     $scope.markers = [];
     $scope.markerId = 1;
@@ -16,7 +19,7 @@ angular
         
         var latlng = new google.maps.LatLng($scope.position.coords.latitude, $scope.position.coords.longitude);
         var myOptions = {
-            zoom: 8,
+            zoom: 17,
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
@@ -24,12 +27,19 @@ angular
         $scope.overlay = new google.maps.OverlayView();
         $scope.overlay.draw = function() {}; // empty function required
         $scope.overlay.setMap($scope.map);
+        var happyIcon = {
+            url: "http://www.freeiconspng.com/uploads/emoji-png-9.png", // url
+            scaledSize: new google.maps.Size(30, 30), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+        };
         var marker = new google.maps.Marker({
             
                             position: latlng,
+                            icon: happyIcon,
                             map: $scope.map,
-                            animation: google.maps.Animation.BOUNCE
+                            animation: google.maps.Animation.DROP
                       });
     };
-    
+    google.maps.event.addDomListener(window, 'load', init);
     });
