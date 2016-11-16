056e80b224824441ea378ee501a1d65f74599b6e
Natalie Chin <chin_natalie@yahoo.com.sg>
2016-11-16 17:55:31 -0600

    added auto location update (every min)

--- app/wndr/index.js
+++ app/wndr/index.js
@@ -1,5 +1,85 @@
 var wndr = angular.module('wndr', [
   // Declare any module-specific AngularJS dependencies here
   'supersonic',
-  ]);
-  
+]);
+
+wndr.constant('icons', {
+    heart: {
+        url: "/emojis/heavy-black-heart.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    angry: {
+        url: "/emojis/Very_Angry_Emoji.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    sad: {
+        url: "/emojis/Very_sad_emoji_icon_png.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    tongueOut: {
+        url: "/emojis/Tongue_Out_Emoji_with_Winking_Eye.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    happyTears: {
+        url: "/emojis/Tears_of_Joy_Emoji.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    thinking: {
+        url: "/emojis/Thinking_Face_Emoji.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    smiling: {
+        url: "/emojis/Smiling_Emoji_with_Eyes_Opened.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0)
+    },
+    grinning: {
+        url: "/emojis/Grinning_Emoji_with_Smiling_Eyes.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    },
+    poop: {
+        url: "/emojis/Poop_Emoji.png",
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    },
+    upside_down: {
+        url: "/emojis/Upside-Down_Face_Emoji.png", // url
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    },
+    OMG: {
+        url: "/emojis/OMG_Face_Emoji.png", // url
+        scaledSize: new google.maps.Size(30, 30), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    },
+    unamused: {
+        url: "/emojis/Unamused_Face_Emoji.png",
+        scaledSize: new google.maps.Size(30, 30),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0)
+    },
+    red_flag: {
+        url: "/emojis/Red_Flag_Emoji.png",
+        scaledSize: new google.maps.Size(30, 30),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 0)
+    }
+});
--- app/wndr/scripts/indexController.js
+++ app/wndr/scripts/indexController.js
@@ -1,92 +1,10 @@
 angular
   .module('wndr')
-  .controller('indexController', function ($scope, supersonic, $compile,$window) {
+  .controller('indexController', function (icons, $interval, $scope, supersonic, $compile,$window) {
 
     $scope.markers = [];
     $scope.currentPosition = undefined;
     $scope.commentInput = "";
-
-    var icons = {
-        heart: {
-          url: "/emojis/heavy-black-heart.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-        },
-        angry: {
-          url: "/emojis/Very_Angry_Emoji.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-        },
-        sad: {
-          url: "/emojis/Very_sad_emoji_icon_png.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-        },
-        tongueOut: {
-          url: "/emojis/Tongue_Out_Emoji_with_Winking_Eye.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-        },
-        happyTears: {
-          url: "/emojis/Tears_of_Joy_Emoji.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-        },
-        thinking: {
-          url: "/emojis/Thinking_Face_Emoji.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-        },
-         smiling: {
-          url: "/emojis/Smiling_Emoji_with_Eyes_Opened.png",
-          scaledSize: new google.maps.Size(30, 30), // scaled size
-          origin: new google.maps.Point(0, 0), // origin
-          anchor: new google.maps.Point(0, 0)
-         },
-         grinning: {
-             url: "/emojis/Grinning_Emoji_with_Smiling_Eyes.png",
-             scaledSize: new google.maps.Size(30, 30), // scaled size
-             origin: new google.maps.Point(0, 0), // origin
-             anchor: new google.maps.Point(0, 0) // anchor
-         },
-         poop: {
-             url: "/emojis/Poop_Emoji.png",
-             scaledSize: new google.maps.Size(30, 30), // scaled size
-             origin: new google.maps.Point(0, 0), // origin
-             anchor: new google.maps.Point(0, 0) // anchor
-         },
-         upside_down: {
-             url: "/emojis/Upside-Down_Face_Emoji.png", // url
-             scaledSize: new google.maps.Size(30, 30), // scaled size
-             origin: new google.maps.Point(0, 0), // origin
-             anchor: new google.maps.Point(0, 0) // anchor
-         },
-         OMG: {
-             url: "/emojis/OMG_Face_Emoji.png", // url
-             scaledSize: new google.maps.Size(30, 30), // scaled size
-             origin: new google.maps.Point(0, 0), // origin
-             anchor: new google.maps.Point(0, 0) // anchor
-         },
-         unamused: {
-             url: "/emojis/Unamused_Face_Emoji.png",
-             scaledSize: new google.maps.Size(30, 30),
-             origin: new google.maps.Point(0, 0),
-             anchor: new google.maps.Point(0, 0)
-         },
-         red_flag: {
-             url: "/emojis/Red_Flag_Emoji.png",
-             scaledSize: new google.maps.Size(30, 30),
-             origin: new google.maps.Point(0, 0),
-             anchor: new google.maps.Point(0, 0)
-         }
-     };
-    localStorage.setItem('icons', icons);
 
      var mapIcon = function (iconName) {
 
@@ -330,4 +248,12 @@ angular
         $scope.currentPosition.setPosition(LatLng);
       });
     });
+
+    var updatePosition = function () {
+        supersonic.device.geolocation.getPosition().then(function (position) {
+            var LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            $scope.currentPosition.setPosition(LatLng);
+        });
+    };
+    $interval(updatePosition, 60000);
 });
